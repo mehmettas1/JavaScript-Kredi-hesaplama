7dea59e44236fe3456bc912550bef718177b9304
mehmet <tas349873@gmail.com>
2022-06-02 02:57:30 +0300

    js

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log('*****Kredi Hesaplama**********');
+
+const select = document.querySelector('.form-select');
+const vade = document.querySelector('#vade');
+const tutar = document.querySelector('#tutar');
+const hesaplaBtn = document.querySelector('.btn-dark');
+let oran =0;
+let taksit = 0
+
+hesaplaBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    if(select.value ==="Konut Kredisi"){
+     oran = 1.29;
+    }else if(select.value ==="Ihtiyac Kredisi"){
+        oran = 1.99;
+    }else if(select.value ==="Arac Kredisi"){
+        oran = 1.79;
+    }
+    if(!select.value || !vade.value ||tutar.value){
+        alert("Lütfen Kredi turu, Vade ve tutari giriniz");
+    }
+    const faiz = oran/100;
+    taksit = tutar.value *(faiz * (1+faiz)**vade.value)/((1+faiz)**vade.value - 1)
+})
