48b8472a929140939ea716d4d6af63e14519daa7
mehmet <tas349873@gmail.com>
2022-06-02 03:17:03 +0300

    js

--- app.js
+++ app.js
@@ -20,5 +20,34 @@ hesaplaBtn.addEventListener("click",(e)=>{
         alert("Lütfen Kredi turu, Vade ve tutari giriniz");
     }
     const faiz = oran/100;
-    taksit = tutar.value *(faiz * (1+faiz)**vade.value)/((1+faiz)**vade.value - 1)
-})
+    taksit = tutar.value *(faiz * (1+faiz)**vade.value)/((1+faiz)**vade.value - 1);
+    sonuclariGoster();
+});
+
+const sonuclariGoster = () => {
+    const sonuclariGoster = document.querySelector('.sonuclar');
+
+   sonuclar.innerHTML = `<table class="table table-bordered border-warning">
+     <tbody>
+    <tr>
+      <th>Kredi Miktarı</th>
+      <td>${tutar.value}</td>
+      <th>Kredi Tipi</th>
+      <td>${select.value}</td>
+    </tr>
+    <tr>
+      <th>Vade</th>
+      <td>${vade.value}</td>
+      <th>Faiz oranı</th>
+      <td>${oran}</td>
+    </tr>
+    <tr>
+      <th>Toplam Tutar</th>
+      <td>${taksit*vade.value}</td>
+      <th>Taksit Tutari</th>
+      <td>${taksit}</td>
+    </tr>
+  </tbody>
+
+</table>
+}
